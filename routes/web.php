a9d6e1442e6407e0d50b55e84c52b2fc917f00c3
Ryan Chandler <ryangjchandler@gmail.com>
2021-08-28 18:33:13 +0100

    chore: setup octane

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
